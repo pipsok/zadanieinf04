3c58f681f80d6d01ff7d83d2a66abf0ddceb0778
pipsok <pawelquick@gmail.com>
2025-09-30 08:24:28 +0200

    zadanieinf04

---
+++ app/src/main/java/com/example/zadanieegzpawel/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.zadanieegzpawel;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textViewPunkty;
+    private Button polub;
+    private Button usun;
+    private int liczbaPunktow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textViewPunkty = findViewById(R.id.textView1);
+        polub  = findViewById(R.id.polub);
+        usun  = findViewById(R.id.usun);
+        polub.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        liczbaPunktow++;
+                        textViewPunkty.setText(Integer.toString(liczbaPunktow)+" polubień");
+                    }
+                }
+        );
+        usun.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (liczbaPunktow > 0){
+                            liczbaPunktow--;
+                        }
+                        textViewPunkty.setText(Integer.toString(liczbaPunktow)+" polubień");
+                    }
+                }
+        );
+    }
+}
